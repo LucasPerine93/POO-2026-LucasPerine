607ff51e1b3c49ae70687b11525e2c91b4d4648e
Seu Nome <seu.email@example.com>
2026-09-18 19:23:38 -0300

    Versão atualizada do projeto final, com bugs corrigidos e mais proteções contra dados falhos

--- projeto_final/locacao_veiculos.py
+++ projeto_final/locacao_veiculos.py
@@ -1,6 +1,6 @@
 from abc import ABC, abstractmethod
 
-class Veiculos:
+class Veiculo:
     def __init__(self, modelo, placa, valor_diaria):
         self.__modelo = modelo
         self.__placa = placa
@@ -16,6 +16,9 @@ class Veiculos:
         return self.__valor_diaria
 
     def set_modelo(self, novo_modelo: str):
+        if not isinstance(novo_modelo, str):
+            raise TypeError("O valor informado deve ser um texto!")
+        
         if novo_modelo != self.__modelo:
             print(f"Modelo: {self.__modelo} alterado para {novo_modelo} com sucesso")
             self.__modelo = novo_modelo
@@ -23,6 +26,9 @@ class Veiculos:
             raise ValueError(f"[ERRO]: O modelo {self.__modelo} ja tem esse nome, para alterar coloque um dado diferente")
 
     def set_placa(self, nova_placa: str):
+        if not isinstance(nova_placa, str):
+            raise TypeError("O valor informado deve ser um texto!")
+        
         if nova_placa != self.__placa:
             print(f"Modelo: {self.__placa} alterado para {nova_placa} com sucesso")
             self.__placa = nova_placa
@@ -31,6 +37,9 @@ class Veiculos:
             raise ValueError(f"[ERRO]: A placa {self.__placa} ja tem esse codigo, para alterar coloque um codigo diferente")
 
     def set_valor_diaria(self, novo_valor: int):
+        if not isinstance(novo_valor, int):
+            raise TypeError("O valor informado deve ser um numero!")
+        
         if novo_valor > 0:
             print(f"Valor: R${self.__valor_diaria:.2f} alterado para R${novo_valor:.2f} com sucesso")
             self.__valor_diaria = novo_valor
@@ -39,17 +48,20 @@ class Veiculos:
             raise ValueError(f"[ERRO]: O valor da diaria deve ser maior que 0")
 
     @abstractmethod
-    def calcular_aluguel(self):
+    def calcular_aluguel(self, dias):
         pass
 
-class Carro(Veiculos):
+class Carro(Veiculo):
     def __init__(self, modelo, placa, valor_diaria, portas):
         super().__init__(modelo, placa, valor_diaria)
 
         self.portas = portas
         self.__taxa_fixa = 50
 
-    def calcular_aluguel(self, dias: int):
+    def calcular_aluguel(self, dias):
+        if not isinstance(dias, int):
+            raise TypeError("O valor informado deve ser um numero!")
+        
         if dias > 0:
             valor = (dias * self.get_valor_diaria()) + self.__taxa_fixa
 
@@ -62,29 +74,33 @@ class Carro(Veiculos):
             print("----------------------")
 
         else:
-            raise ValueError(f"Valor menor que {self.get_valor_diaria} (preço da diaria) ")
+            raise ValueError(f"Os dias selecionados são menores ou iguais a 0")
 
-class Moto(Veiculos):
+class Moto(Veiculo):
     def __init__(self, modelo, placa, valor_diaria, cilindradas):
         super().__init__(modelo, placa, valor_diaria)
 
         self.cilindradas = cilindradas
         self.__taxa_fixa = 10
 
-    def calcular_aluguel(self, dias: int):
+    def calcular_aluguel(self, dias):
+        if not isinstance(dias, int):
+            raise TypeError("O valor informado deve ser um numero!")
+        
         if dias > 0:
-            valor = (dias * self.get_valor_diaria()) * (self.__taxa_fixa / 100)
+            valor = (dias * self.get_valor_diaria())
+            valor_com_taxa = valor - ((self.__taxa_fixa / 100) * valor)
 
             print(f"\nA moto de modelo: {self.get_modelo()}, foi alugado por {dias}  dias")
             print("------- RESUMO -------")
-            print(f"Valor alugado: {valor:.2f}")
-            print(f"Taxa de: {self.__taxa_fixa}%")
+            print(f"Valor alugado: {valor_com_taxa:.2f}")
+            print(f"Taxa de desconto: {self.__taxa_fixa}%")
             print(f"Dias com a moto: {dias} dias")
             print(f"Configuracao: moto com {self.cilindradas} cilindradas")
             print("----------------------")
         
         else:
-            raise ValueError(f"Valor menor que {self.get_valor_diaria} (preço da diaria)")
+            raise ValueError(f"Os dias selecionados são menores ou iguais a 0")
 
 veiculos = [
     Carro("Corola", "BHU-0935", 150, 4),
@@ -94,8 +110,8 @@ veiculos = [
 ]
 
 def exibir_opcoes():
-    print("\n1. Simular folha de pagamento de todos")
-    print("0. Sair")
+    print("\n1. Simular aluguel da frota inteira")
+    print("0. Sair\n")
 
 
 def main():
@@ -104,24 +120,29 @@ def main():
 
         try:
             opcao = int(input("Digite um numero: "))
-        except ValueError:
+        except (ValueError, TypeError) as e:
             print("[ERRO]: Digite apenas numeros inteiros e validos\n")
             continue
 
         if opcao == 1:
             try:
                 dias = int(input("Para quantos dias: "))
-            except ValueError:
+            except (ValueError, TypeError) as e:
                 print("[ERRO]: Digite apenas numeros inteiros e validos\n")
+                continue
 
-            for i in veiculos:
-                i.calcular_aluguel(dias)
+            try:
+                 for i in veiculos:
+                    i.calcular_aluguel(dias)
+            except (ValueError, TypeError) as e:
+                print(e)
+
+        elif opcao == 0:
+            print("Saindo...")
+            break
 
         else:
             print("[ERRO]: Digite apenas numeros inteiros e validos\n")
-
-        if opcao == 0:
-            break
 
 if __name__ == "__main__":
     main()
